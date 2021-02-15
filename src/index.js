12f4c9197e0f8410b172226fb4b538af823671f6
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-02-15 13:49:47 +0100

    test baner

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import 'bootstrap';
 import './styles/style.scss';
 
 import menu from './scripts/menu.js';
-
+import hideNavbar from './scripts/hideNavbar.js';
+import typing from './scripts/typing.js';
 
 menu()
+hideNavbar()
+typing()
