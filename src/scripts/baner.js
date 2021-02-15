888c3d301ba2ed7775dfb7849ffbd99e8aff2872
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-02-15 14:44:48 +0100

    fix hiding baner in landscape phone

--- src/scripts/baner.js
+++ src/scripts/baner.js
@@ -1,12 +1,19 @@
 const baner = () =>{
-    const banerText = document.querySelector('.baner-text-P')
-    console.log(screen.availHeight)
-    console.log(screen.availWidth)
-    if(screen.innerWidth < 600){
-        if(screen.availHeight > screen.availWidth){
-            banerText.style.display = "none";
+    const changeScreen = () =>{
+        const banerText = document.querySelector('.baner-text-P')
+        console.log(`width: ${window.innerWidth}`)
+        console.log(`height: ${window.innerHeight}`)
+        if(window.innerWidth < 600){
+            if(window.innerHeight < window.innerWidth){
+                banerText.style.display = "none";
+            }else{
+                banerText.style.display = "flex";
+            }
+        }else{
+            banerText.style.display = "flex";
         }
     }
+    window.onresize = changeScreen
 }
 
 export default baner
