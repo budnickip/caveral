ca2ada39f614bf0ac1370df1ebd23b84cd4785ed
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-02-19 16:40:32 +0100

    working counter

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import menu from './scripts/menu.js';
 import hideNavbar from './scripts/hideNavbar.js';
 import typing from './scripts/typing.js';
 import baner from './scripts/baner.js';
+import counter from './scripts/counter.js';
+
 
 menu()
 hideNavbar()
 typing()
 baner()
+counter()
---
+++ src/scripts/counter.js
@@ -0,0 +1,93 @@
+const counter = () =>{
+    let yearDisplayed = 0
+    let yearloaded = false;
+
+    let customersDisplayed = 0
+    let customersLoaded = false;
+
+    let employersDisplayed = 0
+    let employersLoaded = false;
+
+    let officesDisplayed = 0
+    let officesLoaded = false
+    window.addEventListener('scroll',()=>{
+        const year = document.querySelector('.about-counter__bigHeader')
+        const yearValue = 11;
+
+        if((year.getBoundingClientRect().top < window.innerHeight) && !yearloaded){
+            yearloaded = true;
+            let yearInterval = setInterval(() => {
+                  if(yearDisplayed <= yearValue){
+                      yearDisplayed++
+                      year.innerHTML = yearDisplayed
+                  }
+                  if(yearDisplayed > yearValue){
+                      clearInterval(yearInterval)
+                  }
+                  yearDisplayed++
+              }, 200);
+        }
+
+
+        const customers = document.querySelector('.customers__header')
+        const customersValue = 100;
+
+        if((customers.getBoundingClientRect().top < window.innerHeight) && !customersLoaded){
+            customersLoaded = true;
+
+            let customersInterval = setInterval(() => {
+                if(customersDisplayed <= customersValue){
+                    customers.innerHTML = `${customersDisplayed}%`
+                    customersDisplayed++
+                }
+                customersDisplayed++
+                if(customersDisplayed > customersValue){
+                    clearInterval(customersInterval)
+                }
+            }, 50);
+        }
+
+
+        const employers = document.querySelector('.employers__header')
+        const employersValue = 100;
+
+
+        if((employers.getBoundingClientRect().top < window.innerHeight) && !employersLoaded){
+            employersLoaded = true;
+
+            let employersInterval = setInterval(() => {
+                if(employersDisplayed <= employersValue){
+                    employers.innerHTML = `+${employersDisplayed}`
+                    employersDisplayed++
+                }
+                employersDisplayed++
+                if(employersDisplayed > employersValue){
+                    clearInterval(employersInterval)
+                }
+            }, 50);
+        }
+
+
+        const offices = document.querySelector('.offices__header')
+        const officesValue = 4;
+
+        if((offices.getBoundingClientRect().top < window.innerHeight) && !officesLoaded){
+            officesLoaded = true;
+
+            let officesInterval = setInterval(() => {
+                if(officesDisplayed <= officesValue){
+                    offices.innerHTML = `${officesDisplayed}`
+                    officesDisplayed++
+                }else{
+                    officesDisplayed++
+                }
+                if(officesDisplayed > officesValue){
+                    clearInterval(officesInterval)
+                }
+            }, 450);
+        }
+
+    })
+}
+
+export default counter
